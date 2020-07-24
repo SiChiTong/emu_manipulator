c9acd54fff297fb311510826e88f01ba1ba3f600
pokpongc <c.thanapong@aol.com>
2020-07-25 00:20:16 +0700

    add moveOne moving checj

--- emu_core/scripts/core.py
+++ emu_core/scripts/core.py
@@ -27,7 +27,7 @@ import pyemu
 rospy.init_node('core')
 
 class Core:
-	def __init__(self, _port = '/dev/ttyTHS1'):
+	def __init__(self, _port = '/dev/ttyTHS1', vision = 0):
 		self.preconfig_pose = {'home': [0, 0, pi/2, pi/2, 0, -pi/4], 
 			'bin_snap': [0, 1.2, -0.05, pi/2-0.05, 0, 0], 
 			'tray_left_1': [pi/2, 0.09, 0.1503, 1.6, 0, 0],
@@ -51,7 +51,7 @@ class Core:
 
 		self.__initPublisher()
 		self.__initSubscriber()
-		# self.__initVision()
+		if vision: self.__initVision()
 
 		
 	def __initVison(self):
@@ -103,18 +103,20 @@ class Core:
 	def getStates(self):
 		return self.joint_msg.position
 
-	def notMoving(self, start_pose, goal_pose, tol):
-		pose_now = self.getStates()
-		d = [0,0,0,0,0,0]
-		dtm = [0,0,0,0,0,0]
-		for i in range(0,6):
-			dtm[i] = abs(abs(goal_pose[i]) - abs(start_pose[i]))
-			d[i] = abs(abs(pose_now[i]) - abs(start_pose[i]))
-		print (sum(d), sum(dtm))
-		if sum(d) < tol*sum(dtm):
-			return 1
-		else:
-			return 0
+	def notMoving(self, start_pose, goal_pose, tol, joint = None):
+		if len(start_pose) == 6:
+			pose_now = self.getStates()
+			d = [0]*6
+			dtm = [0]*6
+			for i in range(0,6):
+				dtm[i] = abs(abs(goal_pose[i]) - abs(start_pose[i]))
+				d[i] = abs(abs(pose_now[i]) - abs(start_pose[i]))
+			return 1 if sum(d) < tol*sum(dtm) else 0
+		elif len(start_pose) == 1:
+			pose_now = self.getStates()[joint-1]
+			d = abs(abs(goal_pose) - abs(start_pose))
+			dtm = abs(abs(pose_now) - abs(start_pose))
+			return 1 if d < tol*dtm else 0
 	
 	def __execute(self, cmd):
 		cmd_object = 'ret = self.'+cmd.data
@@ -145,136 +147,24 @@ class Core:
 		return 1
   
 	def moveOne(self, joint, goal, t):
+		print ('Starting to move '+str(joint)+' to ' +  str(goal))
+		s_pose = self.getStates()[joint-1]
 		self.emulator.moveAbsolute(joint, goal, t)
-	
-	def moveDelay(self, last_pose, num_check, t, last_cmd):
-		for _ in range(0, num_check):
-			a = self.notMoving(last_pose, 0.1)
-			if a == 1:
-				exec(last_cmd)
-			self.log(a)
-			time.sleep(t/num_check)
+		while self.notMoving(s_pose, goal, 0.05):
+			time.sleep(0.2)
+			self.log('1')
+			self.emulator.moveAbsolute(joint, goal, t)
+		return 1
   
 	def run(self, script):
-		self.log('Hello There!')
-		last_pose = self.getStates()
-		self.moveTo(self.preconfig_pose['bin_snap'],8)
-		self.moveDelay(last_pose, 5, 10, 'self.moveTo(self.preconfig_pose[\'bin_snap\'],10)')
-		self.log('Start snap bin')
-
-		# # self.vision.snapBinImg()
-		# # self.log('Snap bin finish')
-		# # self.log('find bin pose')
-		# # binMsg = self.vision.getBin()
-		# # self.bin_pose_publisher.publish(binMsg)
-  
-		self.log('Bin position: Done!')
-		time.sleep(0.5)
-		self.log('Moving to left tray 1')
-		last_pose = self.getStates()
-		self.moveTo(self.preconfig_pose['tray_left_1'],16)
-		self.moveDelay(last_pose, 8, 18, 'self.moveTo(self.preconfig_pose[\'tray_left_1\'],17)')
-		self.log('snap pano left 1')
-		self.vision.snapPano('l')
-		self.log('Moving to left tray 2')
-		last_pose = self.getStates()
-		self.moveOne(4, pi/2, 5)
-		self.moveDelay(last_pose, 4, 6, 'self.moveOne(4, pi/2, 3)')
-		self.log('snap pano left 2')
-		self.vision.snapPano('l')
-		self.log('Moving to left tray 3')
-		last_pose = self.getStates()
-		self.moveOne(4, (pi/2)-0.2-0.5, 5)
-		self.moveDelay(last_pose, 4, 6, 'self.moveOne(4, (pi/2)-0.2-0.5, 3)')
-		self.log('snap pano left 3')
-		self.vision.snapPano('l')
-		self.log('start Predict')
-		leftTrashMsg,leftTrayImg,leftTrashNum = self.vision.findTrash('l',panomode=0,persmode=0)
-		self.log('Left tray: Done!  {} '.format(leftTrashNum))
-
-		self.log('Moving to right tray 1')
-		last_pose = self.getStates()
-		self.moveTo(self.preconfig_pose['tray_right_1'],10)
-		self.moveDelay(last_pose, 7, 15, 'self.moveTo(self.preconfig_pose[\'tray_right_1\'],12)')
-		self.log('snap pano right 1')
-		self.vision.snapPano('r')
-		self.log('Moving to right tray 2')
-		last_pose = self.getStates()
-		self.moveOne(4, pi/2, 5)
-		self.moveDelay(last_pose, 4, 6, 'self.moveOne(4, pi/2, 3)')
-		self.log('snap pano right 2')
-		self.vision.snapPano('r')
-		self.log('Moving to right tray 3')
-		last_pose = self.getStates()
-		self.moveOne(4, (pi/2)-0.2-0.05, 5)
-		self.moveDelay(last_pose, 4, 6, 'self.moveOne(4, (pi/2)-0.2-0.05, 3)')
-		self.log('snap pano right 3')
-		self.vision.snapPano('r')
-		self.log('start Predict')
-
-		rightTrashMsg,rightTrayImg,rightTrashNum = self.vision.findTrash('r',panomode=0,persmode=0)
-		self.log('Right tray: Done!  {} '.format(rightTrashNum))
-  
-		if leftTrashNum + rightTrashNum == 0:
-			pass
-		else:
-			if leftTrashNum != 0:
-				self.left_tray_publisher.publish(leftTrayImg)
-				self.trash_poses_publisher.publish(leftTrashMsg)
-
-				time.sleep(0.1)
-			else:
-				# self.left_tray_publisher.publish(leftTrayImg)
-				pass
-			time.sleep(0.1)
-			if rightTrashNum != 0:
-				self.right_tray_publisher.publish(rightTrayImg)
-				self.trash_poses_publisher.publish(rightTrashMsg)		
-
-				time.sleep(0.1)
-			else:
-				# self.right_tray_publisher.publish(rightTrayImg)
-				pass
-		last_pose = self.getStates()
-		config_togo = self.pickTrashCfg(leftTrashMsg)
-		for i in config_togo:
-			self.moveTo(i, 15)
-			self.moveDelay(last_pose, 5, 18, 'self.moveTo(i, 15)')
-			self.moveTo(self.preconfig_pose['home'], 15)
-			self.moveDelay(last_pose, 5, 18, 'self.moveTo(self.preconfig_pose[\'home\'], 15)')
-
-		
-
-		# time.sleep(1)
-		# self.log ('Moving to home')
-		# self.moveTo(self.preconfig_pose['home'], 15)
-		# time.sleep(16)
-		# self.log ('Done!')
-		
-		# script_path = this_pkg+'/scripts/'+script
-		# script_in = open(script_path)
-		# data = script_in.read()
-		# script_in.close()
-		# cmd_list = data.split('\n')
-		# ret = None
-		# for cmd in cmd_list:
-		# 	cmd_object = 'ret = self.'+cmd
-		# 	cmd_static = 'ret = '+cmd
-		# 	try:
-		# 		exec(cmd_object)
-		# 		if ret is None:
-		# 			pass
-		# 		else:
-		# 			rospy.logdebug('Return: '+str(ret))
-		# 	except AttributeError:
-		# 		exec(cmd_static)
-		# 	except Exception as e:
-		# 		print (e)
-		# 		rospy.logerr('Wrong Input: '+e)
-		# 	if ret is None:
-		# 			pass
-		# 	else:
-		# 		rospy.logdebug('Return: '+str(ret))
+		script_path = this_pkg+'/scripts/'+script
+		script_in = open(script_path)
+		data = script_in.read()
+		script_in.close()
+		try:
+			exec(data)
+		except Exception as e:
+			rospy.logerr('Wrong Input: %s'%e)
 
 
 	def publishJointStates(self):
@@ -289,7 +179,6 @@ class Core:
 					self.joint_states_publisher.publish(self.joint_msg)
 
 	def cartesianJogCallback(self, s_twist):
-		self.lock = 1
 		time.sleep(0.01)
 		t_ang = s_twist.angular
 		t_lin = s_twist.linear
@@ -306,10 +195,8 @@ class Core:
 		time.sleep(0.05)
 		self.log('Output joint increment: '+str(dq))
 		self.emulator.moveRelative('all', dq, 5)
-		self.lock = 0
 
 	def configJogCallback(self, state):
-		self.lock = 1
 		time.sleep(0.01)
 		position_array = [state.position[0], state.position[1], state.position[2], state.position[3], state.position[4], state.position[5]]
 		for n,i in enumerate(position_array):
@@ -321,15 +208,12 @@ class Core:
 		print (jointNum, increment)
 		self.log('Input joint increments: '+str(position_array))
 		self.emulator.moveRelative(jointNum, increment, 3)
-		self.lock = 0
 
 if __name__ == "__main__":
-	emu = Core()
-	# emu.initVison()
+	emu = Core(vision = 0)
 	emu.log('emu core initialized successfully', None)
 	rospy.logdebug('emu core initialized successfully')
 	joint_pub_thread = threading.Thread(target=emu.publishJointStates, daemon = 1)
 	joint_pub_thread.start()
 	while not rospy.is_shutdown():
-		# print ('in while')
 		time.sleep(1)
